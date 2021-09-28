2d0b72ef639880c86b46a04a8b1da40bbdc0c4b8
Ryan Harrison <ryan.harrison@student.manchester.ac.uk>
2021-09-28 19:04:09 +0100

    Create voter model

--- src/society_elections/models.py
+++ src/society_elections/models.py
@@ -241,3 +241,52 @@ class Candidate(models.Model):
     def __str__(self):
         return f'{self.full_name} for {self.position.position}'
 
+
+class Voter(models.Model):
+    f"""Represents a voter in the database
+
+    Data is anonymized as much as possible here, by creating a hash with a salt 
+    of the email address that is used to signup. This prevents double voting 
+    from occuring
+    
+    Attributes:
+        id (UUID): Unique identifier for a voter in the database. A UUID is 
+            used to prevent guessable voter IDs
+        election ({Election.__name__}): The election the voter is signed up to
+            vote in
+        email_salt (bytes): Salt used when hashing a voter's email
+        email_hash (bytes): The hash of the email combined with the salt
+        verified (bool): Has the voter verified their email address
+        registered_at (datetime): The time the voter registered
+        verified_at (datetime): When the voter verified their email
+    """
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    election = models.ForeignKey(
+        to=f'{SocietyElectionsConfig}.{Election.__name__}',
+        on_delete=models.CASCADE,
+        related_name='voters',
+        related_query_name='voter'
+    )
+    email_salt = models.BinaryField()
+    email_hash = models.BinaryField()
+    verified = models.BooleanField(
+        default=False,
+        editable=False
+    )
+    registered_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+    verified_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        editable=False
+    )
+
+    def __str__(self):
+        return str(self.id)
+
