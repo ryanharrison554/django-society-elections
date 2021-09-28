3df103652f82db4ba33120f9c8cf42f46d8ae389
Ryan Harrison <ryan.harrison@student.manchester.ac.uk>
2021-09-28 19:03:57 +0100

    Create election position model

--- src/society_elections/models.py
+++ src/society_elections/models.py
@@ -152,3 +152,92 @@ class Position(models.Model):
 
     def __str__(self):
         return self.admin_title
+
+
+class ElectionPosition(models.Model):
+    """Specifies a position to allow candidates to run for in a given election
+
+    Essentially implements a many-to-many relation, with some additional fields
+
+    Attributes:
+        position (Position): The position to include
+        election (Election): Election the position is to be included in
+        candidate_required (bool): Whether or not this position requires a 
+            candidate in order for a successful election to take place
+        allow_ron (bool): Allow voters to vote to "Re-open Nominations" when 
+            voting for this position
+        allow_abstain (bool): Allow votes to abstain from voting for a 
+            candidate for this position
+        positions_available (int): How many nominees can be successful in 
+            applying for this position 
+    """
+    position = models.ForeignKey(
+        to=f'{SocietyElectionsConfig.name}.Position',
+        on_delete=models.CASCADE
+    )
+    election = models.ForeignKey(
+        to=f'{SocietyElectionsConfig.name}.Election',
+        on_delete=models.CASCADE,
+        related_name='positions',
+        related_query_name='position'
+    )
+    candidate_required = models.BooleanField(
+        default=True,
+        help_text='Whether or not at least one candidate is required in order '
+        'to run this election'
+    )
+    allow_ron = models.BooleanField(
+        default=True,
+        help_text='Whether or not to include "Re-open Nominations" in the list '
+        'of options for voters'
+    )
+    allow_abstain = models.BooleanField(
+        default=True,
+        help_text='Whether or not to allow voters to abstain from voting for '
+        'this position'
+    )
+    positions_available = models.PositiveIntegerField(
+        default=1,
+        help_text='Number of available positions for this role'
+    )
+
+    def __str__(self):
+        return f'{self.position.title} in {self.election.admin_title}'
+
+
+class Candidate(models.Model):
+    f"""Nominee for a given election position
+    
+    Attributes:
+        position ({ElectionPosition.__name__}): The 
+            {ElectionPosition.__name__} that the candidate is running for
+        full_name (str): Name of the candidate
+        email (str): Email address of the candidate, used to email them the 
+            details of the election and whether or not they have won
+        successful (bool): Whether or not the candidate was successful in their 
+            nomination. Added so that results can be overridden in the case 
+            where a candidate can only hold one position but won two
+        nominated_at (datetime): When the candidate was nominated
+    """
+    position = models.ForeignKey(
+        to=f'{SocietyElectionsConfig.name}.{ElectionPosition.__name__}',
+        on_delete=models.CASCADE,
+        related_name='candidates',
+        related_query_name='candidate'
+    )
+    full_name = models.CharField(
+        max_length=128
+    )
+    email = models.EmailField()
+    successful = models.BooleanField(
+        default=False,
+        editable=False
+    )
+    nominated_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+
+    def __str__(self):
+        return f'{self.full_name} for {self.position.position}'
+
