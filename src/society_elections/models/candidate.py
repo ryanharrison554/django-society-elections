6752b1d1e63db4b9d7bbf1aa699e48d20ce93570
Ryan Harrison <ryan.harrison@student.manchester.ac.uk>
2021-10-02 17:08:26 +0100

    Add email verification methods to candidate model

--- src/society_elections/models/candidate.py
+++ src/society_elections/models/candidate.py
@@ -1,4 +1,9 @@
+import uuid
+
+from django.conf import settings
+from django.core.mail import send_mail
 from django.db import models
+from django.urls import reverse
 
 from ..apps import SocietyElectionsConfig
 from .electionposition import ElectionPosition
@@ -44,6 +49,46 @@ class Candidate(models.Model):
     email_verified = models.BooleanField(
         default=False
     )
+
+
+    def send_verification_email(self):
+        """Send a verification email to the candidate
+        
+        Only send a verification email if this has been configured in the 
+        election settings.
+        """
+        if self.position.election.verify_candidate_email:
+            if self.email_uuid is None:
+                raise ValueError(
+                    'UUID has not yet been set, cannot send verification email'
+                )
+            send_mail(
+                f'Verify Email for Nomination in {self.position.election}',
+                self.position.election.candidate_verification_email.format(
+                    name=self.full_name,
+                    position=self.position.position,
+                    verify_url=self.verify_url
+                )
+            )
+
+
+    @property
+    def verify_url(self) -> str:
+        """str: URL to click to verify the email address of a candidate"""
+        return (
+            f'{settings.ROOT_URL}/'
+            f'{reverse("candidate_verify", args=(self.email_uuid))}'
+        )
+
+    
+    def save(self, *args, **kwargs):
+        """Create a UUID for the verification email if required when saving for 
+        the first time
+        """
+        if self.pk is None and self.position.election.verify_candidate_email:
+            self.email_uuid = uuid.uuid4()
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return f'{self.full_name} for {self.position.position}'
