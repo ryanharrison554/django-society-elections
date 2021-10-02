cce0756a428bef00214344581c21b9cdf5957233
Ryan Harrison <ryan.harrison@student.manchester.ac.uk>
2021-10-02 14:47:19 +0100

    Create PositionAdmin

---
+++ src/society_elections/admin/position.py
@@ -0,0 +1,23 @@
+from logging import getLogger
+
+from django.contrib import admin
+from django.utils.decorators import method_decorator
+
+from ..decorators import log_model_admin_action
+from ..models import Position
+
+logger = getLogger(__name__)
+
+
+@admin.register(Position)
+@method_decorator(
+    log_model_admin_action('save', Position, logger),
+    name='save_model'
+)
+@method_decorator(
+    log_model_admin_action('delete', Position, logger),
+    name='delete_model'
+)
+class PositionAdmin(admin.ModelAdmin):
+    """Defines how the Position model is presented on the admin interface
+    """
