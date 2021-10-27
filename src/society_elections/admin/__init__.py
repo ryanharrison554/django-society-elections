107db298d4303a1da7af9a472bcd0079bef2fd2e
Ryan Harrison <ryan.harrison@student.manchester.ac.uk>
2021-10-27 17:02:13 +0100

    Register voter on admin interface

--- src/society_elections/admin/__init__.py
+++ src/society_elections/admin/__init__.py
@@ -2,3 +2,4 @@ from .candidate import CandidateAdmin
 from .election import ElectionAdmin
 from .electionposition import ElectionPosition
 from .position import PositionAdmin
+from .voter import RegisteredVoterAdmin
---
+++ src/society_elections/admin/voter.py
@@ -0,0 +1,48 @@
+from logging import getLogger
+
+from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
+from django.utils.decorators import method_decorator
+
+from ..decorators import log_model_admin_action
+from ..models import RegisteredVoter
+
+logger = getLogger(__name__)
+
+@admin.register(RegisteredVoter)
+@method_decorator(
+    log_model_admin_action('save', RegisteredVoter, logger),
+    name='save_model'
+)
+@method_decorator(
+    log_model_admin_action('delete', RegisteredVoter, logger),
+    name='delete_model'
+)
+class RegisteredVoterAdmin(admin.ModelAdmin):
+    """Class defining how a registered voter appears on the admin interface
+
+    Attributes:
+        list_display (tuple): How registered voters are grouped by date
+        actions (list): Actions available to perform on the models in the admin 
+            interface
+    """
+    list_display = ('email', 'election', 'verified', 'registered_at')
+    actions = ['resend_verification_email_action']
+
+    @admin.action(description='Resend verification emails')
+    @method_decorator(log_model_admin_action(
+        'resend verification email', RegisteredVoter, logger
+    ))
+    def resend_verification_email_action(
+        self, request: HttpRequest,  queryset: QuerySet
+    ):
+        """Set request-specific data on the object before saving to database
+
+        Args:
+            request (HttpRequest): Request made when saving model
+            obj (Election): Election object to save in database
+        """
+        voter: RegisteredVoter
+        for voter in queryset:
+            voter.send_verification_email()
