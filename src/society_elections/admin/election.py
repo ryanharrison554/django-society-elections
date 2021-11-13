a142bb412ec2884bd004eb3ab27717766c0d2f36
Ryan Harrison <ryan.harrison@student.manchester.ac.uk>
2021-11-13 12:02:29 +0000

    Add actions for election
    Adds actions to end some elections and download their votes
    in csv format

--- src/society_elections/admin/election.py
+++ src/society_elections/admin/election.py
@@ -1,11 +1,16 @@
+import csv
+from io import StringIO
 from logging import getLogger
 
-from django.contrib import admin
-from django.http import HttpRequest
+from django.contrib import admin, messages
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse
+from django.utils import timezone
 from django.utils.decorators import method_decorator
+from django.utils.translation import ngettext
 
 from ..forms import ElectionForm
-from ..models import Election
+from ..models import Election, ElectionPosition, Vote
 from .decorators import log_model_admin_action
 
 logger = getLogger(__name__)
@@ -31,6 +36,71 @@ class ElectionAdmin(admin.ModelAdmin):
         'admin_title', 'nominations_start', 'voting_start', 'ended_at'
     )
     form = ElectionForm
+    actions = ['end_election', 'download_csv_of_votes']
+
+
+    @admin.action(description='End election')
+    @method_decorator(log_model_admin_action(
+        'end election', Election, logger
+    ))
+    def end_election(
+        self, request: HttpRequest, queryset: QuerySet
+    ):
+        """Marks a given set of elections as finished
+
+        Args:
+            request (HttpRequest): Request from staff user
+            queryset (QuerySet): Queryset of elections to finish
+        """
+        queryset.update(
+            ended_at=timezone.now(),
+            ended_by=request.user
+        )
+        messages.add_message(request, messages.SUCCESS,
+            f'Successfully ended {queryset.count()} election'+
+            ngettext('', 's', queryset.count())
+        )
+
+
+    @admin.action(description='Download CSV of votes')
+    @method_decorator(log_model_admin_action(
+        'download csv of votes', Election, logger
+    ))
+    def download_csv_of_votes(
+        self, request: HttpRequest, queryset: QuerySet
+    ):
+        """Download a CSV of all votes cast in the election
+
+        Args:
+            request (HttpRequest): Request from a staff user
+            queryset (QuerySet): Queryset of the elections to fetch votes for
+        """
+        f = StringIO()
+        writer = csv.writer(f)
+        writer.writerow([
+            'voter_id', 'election_id', 'election_admin_title', 'anonymous',
+            'candidate', 'candidate_id', 'vote_cast_at', 'vote_last_modified'
+        ])
+        election: Election
+        position: ElectionPosition
+        vote: Vote
+        for election in queryset:
+            for position in election.positions.all():
+                for vote in position.votes.all():
+                    writer.writerow([
+                        vote.voter_pk,
+                        str(election.pk),
+                        election.admin_title, 
+                        election.anonymous,
+                        str(vote.candidate),
+                        str(vote.candidate.pk),
+                        vote.vote_cast_at.isoformat(),
+                        vote.vote_last_modified_at.isoformat()
+                    ])
+        f.seek(0)
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=votes.csv'
+        return response
 
 
     def save_model(self, request: HttpRequest, obj: Election, *args, **kwargs):
