f48de0961f7c1777e896d89fcd2d244d5e03e5b7
Ryan Harrison <ryan.harrison@student.manchester.ac.uk>
2022-06-03 13:21:09 +0100

    Add additional logging to candidate form

--- src/society_elections/forms/candidate.py
+++ src/society_elections/forms/candidate.py
@@ -1,7 +1,11 @@
+from logging import getLogger
+
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
 from ..models import Candidate, Election, ElectionPosition
+
+logger = getLogger(__name__)
 
 class BaseCandidateForm(ModelForm):
     """Base form for a Candidate providing validation
@@ -17,9 +21,15 @@ class BaseCandidateForm(ModelForm):
         position: ElectionPosition = cleaned_data.get('position')
         election: Election = position.election
 
-        email_whitelist = election.candidate_email_domain_whitelist.split('\n')
-        email_domain = email.split('@')[-1]
+        logger.debug(
+            f'Verifying that "{email}" has a valid domain in the whitelist'
+        )
+
+        logger.debug(f'Whitelist: {email_whitelist}')
+        email_domain = email.split('@')[1]
+        logger.debug(f'Email domain to validate: {email_domain}')
         if email_domain not in email_whitelist:
+            logger.debug(f'Email "{email}" not in valid domains')
             self.add_error('email', ValidationError(
                 'Email not in whitelisted domains for this election'
             ))
